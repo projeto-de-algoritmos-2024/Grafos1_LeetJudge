fd3c4761896569dc30489337218f6bf6a9bd69e3
Ricardo <lrsj2003@gmail.com>
2024-04-15 19:21:54 -0300

    resolvendo problema dos grafos ciclicos

--- 2392.py
+++ 2392.py
@@ -7,20 +7,32 @@ class Solution(object):
         :rtype: List[List[int]]
         """
 
+        # Cria grafo para a ordem das linhas e verifica se é aciclico
         gLinhas = Grafo(k)
-        gColunas = Grafo(k)
-
+        
         for rowCondition in rowConditions:
             gLinhas.adicionaAresta(rowCondition[0],rowCondition[1])
+        
+        if gLinhas.temCiclo():
+            return []
 
+        # Cria grafo para a ordem das colunas e verifica se é aciclico
+        gColunas = Grafo(k)
+        
         for colCondition in colConditions:
             gColunas.adicionaAresta(colCondition[0],colCondition[1])
+        
+        if gColunas.temCiclo():
+            return []
 
+        # Faz ordenação topologica dos grafos
         ordemLinhas = gLinhas.ordemTopologica()
         ordemColunas = gColunas.ordemTopologica()
 
+        # Cria matriz de zeros
         matriz = [[0] * k for _ in range(k)]
 
+        # Preenche matriz de zeros com numeros obedecendo a ordem topologica
         for i in range(1, k+1):
             m = ordemLinhas.index(i)
             n = ordemColunas.index(i)
@@ -28,49 +40,58 @@ class Solution(object):
                 matriz[m][n] = i
             else:
                 return []
-
-        print(matriz)
-        print("==================== Linha ==================== ", gLinhas.ordemTopologica())
-        gLinhas.mostraAdjacencia()
-        print("===================== Coluna ===================== ", gColunas.ordemTopologica())
-        gColunas.mostraAdjacencia()
-
         return matriz
 
-
-
-
 class Grafo:
-
-    def __init__(self, vertices):
-        self.vertices = vertices
-        self.grafo = [[] for i in range(self.vertices)]
+    def __init__(self, k):
+        self.k = k
+        self.grafo = [[] for _ in range(self.k)]
 
     def adicionaAresta(self, u, v):
-        self.grafo[u-1].append(v)
-        # self.grafo[v-1].append(u)
+        self.grafo[u - 1].append(v)
 
-    def dfs(self, v, visitado, stack):
+    def dfs(self, v, visitado, pilhaRecursao):
         visitado[v - 1] = True
+        pilhaRecursao[v - 1] = True
         for vizinho in self.grafo[v - 1]:
             if not visitado[vizinho - 1]:
-                self.dfs(vizinho, visitado, stack)
-        stack.append(v)
+                if self.dfs(vizinho, visitado, pilhaRecursao):
+                    return True
+            elif pilhaRecursao[vizinho - 1]:
+                return True
+        pilhaRecursao[v - 1] = False
+        return False
+
+    def temCiclo(self):
+        visitado = [False] * self.k
+        pilhaRecursao = [False] * self.k
+        for i in range(self.k):
+            if not visitado[i]:
+                if self.dfs(i + 1, visitado, pilhaRecursao):
+                    return True
+        return False
 
     def ordemTopologica(self):
-        visitado = [False] * self.vertices
-        stack = []
-        for i in range(self.vertices):
+        visitado = [False] * self.k
+        ordem = []
+        def dfs_ordem(v):
+            visitado[v - 1] = True
+            for vizinho in self.grafo[v - 1]:
+                if not visitado[vizinho - 1]:
+                    dfs_ordem(vizinho)
+            ordem.append(v)
+        for i in range(self.k):
             if not visitado[i]:
-                self.dfs(i + 1, visitado, stack)
-        return stack[::-1]
+                dfs_ordem(i + 1)
+        return ordem[::-1]
 
     def mostraAdjacencia(self):
-        for i in range(self.vertices):
+        for i in range(self.k):
             print(f'{i+1}:', end='  ')
             for j in self.grafo[i]:
                 print(f'{j}  ->', end='  ')
             print('')
+
 
 sol = Solution()
 sol.buildMatrix(3,[[1,2],[2,3],[3,1],[2,3]],[[2,1]])
